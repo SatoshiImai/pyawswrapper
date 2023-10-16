db13f78c5702138ea3b462381d3b1b7143c96536
Satoshi Imai <satoshi.imai@finatext.com>
2023-10-16 23:53:44 +0900

    athenaclient

--- src/pyawswrapper/__init__.py
+++ src/pyawswrapper/__init__.py
@@ -1,8 +1,11 @@
+from .athenaclient import AthenaCallException, AthenaClient
 from .s3client import ClientErrorException, s3client
 from .s3path import s3path
 
 __all__ = [
+    'AthenaClient',
     's3path',
     's3client',
+    'AthenaCallException',
     'ClientErrorException'
 ]
